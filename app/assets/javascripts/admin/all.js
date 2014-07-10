fe634653f01a413acf23247b300c02fb8874592b
AjayROR <sahu.ajaykumar4@gmail.com>
2014-07-10 19:34:33 +0530

    added the unblock js code

--- app/assets/javascripts/admin/all.js
+++ app/assets/javascripts/admin/all.js
@@ -11,53 +11,79 @@
 //= require ckeditor/init
 //= require jquery_nested_form
 //= require jquery-ui.min
+//= require jquery.blockUI
 //= require_tree .
 
 var ready;
 ready = function() {
-	$("body").on("change", ".each_image",function() {
+  $("body").on("change", ".each_image",function() {
 
-	    var val = $(this).val();
+      var val = $(this).val();
 
-	    switch(val.substring(val.lastIndexOf('.') + 1).toLowerCase()){
-	        case 'gif': case 'jpg': case 'png': case 'x-png': case 'jpeg': case 'pjpeg':
-	            break;
-	        default:
-	            $(this).val('');
-	            // error message here
-	            alert("Please upload only jpeg, jpg or png images.");
-	            break;
-	    }
-	});
+      switch(val.substring(val.lastIndexOf('.') + 1).toLowerCase()){
+          case 'gif': case 'jpg': case 'png': case 'x-png': case 'jpeg': case 'pjpeg':
+              break;
+          default:
+              $(this).val('');
+              // error message here
+              alert("Please upload only jpeg, jpg or png images.");
+              break;
+      }
+  });
 
-/*	$('#order_selection').selectpicker({ 'selectedText': '',style:'btn-lg' }); */
+/*  $('#order_selection').selectpicker({ 'selectedText': '',style:'btn-lg' }); */
 
 
-	$("#coupon_fieldset").on("blur", "input[type='text']", function(){
+  $("#coupon_fieldset").on("blur", "input[type='text']", function(){
         $(this).val($(this).val().split(" ").join(""));
     });
 
-	load_calendar();
+  load_calendar();
 
+    reset_form_for_export();
+    reset_form_for_filteration();
+
+
+    var App = function(){
+  return {
+    blockUI: function(el){
+      el.block({
+        message: '',
+        css: {backgroundColor: 'none'},
+        overlayCSS: {
+          backgroundColor:'#FFFFFF',
+          backgroundImage: "url('/assets/loader.gif')",
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          opacity: 0.67
+        }
+      });
+    },
+    unBlockUI: function(el){
+      el.unblock();
+    }
+  }
+}();
 
 };
+
 $(document).ready(ready);
 $(document).on('page:load', ready);
 
 /*
-	function load_calendar(){
-		console.log("calendar got loaded");
-		$(".order-datepicker").datepicker({
-			changeMonth: true,
-      		changeYear: true
- 		});
+  function load_calendar(){
+    console.log("calendar got loaded");
+    $(".order-datepicker").datepicker({
+      changeMonth: true,
+          changeYear: true
+    });
 
-	}
+  }
 */
 
-	function load_calendar(){
+  function load_calendar(){
 
-		 $( "#date_start" ).datepicker({
+     $( "#date_start" ).datepicker({
       defaultDate: "+1w",
       changeMonth: true,
       numberOfMonths: 1,
@@ -75,6 +101,22 @@ $(document).on('page:load', ready);
         $("#date_start").datepicker( "option", "maxDate", selectedDate );
       }
     });
+  }
 
+    function reset_form_for_export (){
+        $("#form_filter").on("click", "#export_submit", function(){
+            $("#form_filter").attr("action", "/spree/admin/orders/export.xls");
+            $("#form_filter").attr("method", "post");
+            $("#form_filter").removeAttr("data-remote");
+        });
+    }
 
-	}
+    function reset_form_for_filteration(){
+        $("#filter_export").on("click", "#filter_submit", function(){
+            console.log("filter chosen");
+            $("#form_filter").attr("action", "/spree/admin/orders/range");
+            $("#form_filter").attr("method", "get");
+            $("#form_filter").attr("data-remote", "true");
+
+        });
+    }
