bdfea20fac27201368a860d1664963ce329cb5b9
AjayROR <sahu.ajaykumar4@gmail.com>
2014-07-18 14:05:30 +0530

    moving the hourly schedular to git

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,8 +24,14 @@ every :day, :at => '12:20am', :roles => [:app] do
   rake "push_order_to_hub:push_order"
 end
 
+=begin
+every 1.minutes, :roles => [:app] do
+    rake "notification:notify_customer_and_admin"
+    rake "block_subscription:block_expired_subscriptions"
+end
+=end
 
 every :hour, :roles => [:app] do
-    rake "block_subscription:block_expired_subscriptions"
     rake "notification:notify_customer_and_admin"
+    rake "block_subscription:block_expired_subscriptions"
 end
