ee553b5ee0343bb24897911ff94f3a2b45a31d53
AjayROR <sahu.ajaykumar4@gmail.com>
2014-07-21 11:56:01 +0530

    added both local & production credentials

--- config/initializers/mandrill.rb
+++ config/initializers/mandrill.rb
@@ -1,5 +1,6 @@
 
 MandrillMailer.configure do |config|
-  #config.api_key = "Aw9ZnHUPdCH6Tp97gSzVkw"
-  config.api_key = "MpHpaOYBNJp4F-WhluOwAQ"
+
+  config.api_key = "Aw9ZnHUPdCH6Tp97gSzVkw" #enable this for production.
+  #config.api_key = "MpHpaOYBNJp4F-WhluOwAQ"  #enable this for local/staging.
 end
