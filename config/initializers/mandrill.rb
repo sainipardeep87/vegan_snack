a06fca5e255c9fe21f63116be551e96e138967ce
AjayROR <sahu.ajaykumar4@gmail.com>
2014-07-21 19:10:40 +0530

    enabled mandrill account for development & production envionments

--- config/initializers/mandrill.rb
+++ config/initializers/mandrill.rb
@@ -1,6 +1,6 @@
 
 MandrillMailer.configure do |config|
 
-  config.api_key = "Aw9ZnHUPdCH6Tp97gSzVkw" #enable this for production.
-  #config.api_key = "MpHpaOYBNJp4F-WhluOwAQ"  #enable this for local/staging.
+  #config.api_key = "Aw9ZnHUPdCH6Tp97gSzVkw" #enable this for production.
+  config.api_key = "MpHpaOYBNJp4F-WhluOwAQ"  #enable this for local/staging.
 end
