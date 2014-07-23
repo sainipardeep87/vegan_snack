b5f1df5e984b22b6c9fb6b6e57edad46c8a11990
AjayROR <sahu.ajaykumar4@gmail.com>
2014-07-23 17:12:06 +0530

    changed the logic for phone number update from admin login

--- app/controllers/spree/admin/orders/customer_details_controller_decorator.rb
+++ app/controllers/spree/admin/orders/customer_details_controller_decorator.rb
@@ -2,10 +2,10 @@ Spree::Admin::Orders::CustomerDetailsController.class_eval do
     include CustomerDetailsHelper
 
     def update
-        #@order.bill_address.assign_attributes(order_params[:bill_address_attributes])
-        #@order.ship_address.assign_attributes(order_params[:ship_address_attributes])
+        @order.bill_address.assign_attributes(order_params[:bill_address_attributes])
+        @order.ship_address.assign_attributes(order_params[:ship_address_attributes])
 
-        if @order.update_attributes(order_params)
+        if @order.save
             @order.update_total_and_item_total
             flash[:success] = Spree.t('customer_details_updated')
             redirect_to admin_order_customer_path(@order)
@@ -15,6 +15,7 @@ Spree::Admin::Orders::CustomerDetailsController.class_eval do
             end
             render :action => :edit
         end
+
     end
 
 end
